cd2649a707dfdfc82cc47348cf627f7347a7dcb7
Jan Bil <jan.bil@bebio.pl>
2026-01-20 21:16:28 +0100

    Dodaj pliki projektów.

---
+++ tramwaj_wodny/tramwaj_wodny.cpp
@@ -0,0 +1,12 @@
+﻿// tramwaj_wodny.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "tramwaj_wodny.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
