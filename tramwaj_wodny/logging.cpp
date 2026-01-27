696810e8a7753a76cc33bbacfe2881639f486bab
Jan Bil <janbil9991@gmail.com>
2026-01-27 20:50:37 +0100

    feat(logger): base structure

---
+++ tramwaj_wodny/logging.cpp
@@ -0,0 +1,17 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <semaphore.h>
+#include <stdarg.h>
+
+// Prosty logger do pliku (append). U¿ywa semafora do serializacji wpisów.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LOGGING_H
