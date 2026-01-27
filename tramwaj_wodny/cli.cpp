2cf70fd7fb8d2f41e10f7edd637b4d9768b5cbcd
Jan Bil <janbil9991@gmail.com>
2026-01-27 21:27:58 +0100

    feat(cli): implement arg parser

--- tramwaj_wodny/cli.cpp
+++ tramwaj_wodny/cli.cpp
@@ -47,3 +47,49 @@ static void init_defaults(cli_args_t* a) {
     a->interactive = 1;
     snprintf(a->log_path, sizeof(a->log_path), "simulation.log");
 }
+
+int cli_parse_launcher(int argc, char** argv, cli_args_t* out) {
+    if (!out) return -1;
+    init_defaults(out);
+
+    for (int i = 1; i < argc; i++) {
+        const char* k = argv[i];
+        if (streq(k, "--N") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->N) != 0) return -1;
+        }
+        else if (streq(k, "--M") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->M) != 0) return -1;
+        }
+        else if (streq(k, "--K") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->K) != 0) return -1;
+        }
+        else if (streq(k, "--T1") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->T1_ms) != 0) return -1;
+        }
+        else if (streq(k, "--T2") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->T2_ms) != 0) return -1;
+        }
+        else if (streq(k, "--R") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->R) != 0) return -1;
+        }
+        else if (streq(k, "--P") && need_arg(i, argc)) {
+            if (parse_i32(argv[++i], &out->P) != 0) return -1;
+        }
+        else if (streq(k, "--bike-prob") && need_arg(i, argc)) {
+            if (parse_double(argv[++i], &out->bike_prob) != 0) return -1;
+        }
+        else if (streq(k, "--log") && need_arg(i, argc)) {
+            snprintf(out->log_path, sizeof(out->log_path), "%s", argv[++i]);
+        }
+        else if (streq(k, "--help")) {
+            cli_print_usage_tramwaj();
+            return 1;
+        }
+        else {
+            fprintf(stderr, "Unknown arg: %s\n", k);
+            return -1;
+        }
+    }
+
+    return 0;
+}
