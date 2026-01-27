18ea4a42c222e107117b37e1a1de41c8b789eb68
Jan Bil <janbil9991@gmail.com>
2026-01-27 19:15:12 +0100

    feat: define SysV message types for captain-passenger commands

--- tramwaj_wodny/common.h
+++ tramwaj_wodny/common.h
@@ -10,11 +10,14 @@
 extern "C" {
 #endif
 
+// ======= Limity  =======
 // limity dla kompliacji, potem do zmiany kiedy bêd¹ testy dla wiêkszych wartoœci
 enum {MAX_K = 512};
 enum {MAX_P = 1000};
 enum {BRIDGE_Q_CAP = 1024};
 
+
+// ======= Stany i kierunki  =======
 typedef enum {
 	PHASE_LOADING = 0,
 	PHASE_DEPARTING = 1,
@@ -30,11 +33,43 @@ typedef enum {
 
 typedef enum {
 	BRIDGE_DIR_NONE= 0,
-	BRIDGE_DIR_IN = 1,
-	BRIDGE_DIR_OUT = 2
+	BRIDGE_DIR_IN = 1, // l¹d -> statek
+	BRIDGE_DIR_OUT = 2 // statek -> l¹d
 } bridge_dir_t;
 
 
+// ======= Kolejka/deque na mostku =======
+// W normalnym ruchu:
+/// DIR_IN  : push_back (wejœcie na mostek od strony l¹du), pop_front (wejœcie na statek)
+/// DIR_OUT : push_front (wejœcie na mostek od strony statku), pop_back (zejœcie na l¹d)
+//
+// Przy odp³ywaniu: kapitan wymusza DIR_OUT i usuwa z back (LIFO).
+
+typedef struct {
+	pid_t pid;
+	uint8_t units; // 1 albo 2(rower)
+	uint8_t evicting; // czy pasa¿er jest usuwany przez kapitana przy odp³ywaniu
+} bridge_node_t;
+
+// ======= Komunikaty (SysV msgqueue) =======
+// Kapitan -> pasa¿er: mtype = PID pasa¿era
+// Pasa¿er -> kapitan: mtype = 1
+
+typedef enum {
+	CMD_EVICT = 1
+} cmd_t;
+
+typedef struct {
+	long mtype;          // PID adresata
+	cmd_t cmd;
+	int32_t trip_no;
+} msg_cmd_t;
+
+typedef struct {
+	long mtype;          // 1
+	pid_t pid;
+	int32_t trip_no;
+} msg_ack_t;
 
 #ifdef __cplusplus
 }
