049ccf743f4265ddecb047272398eb07a23c569d
Jan Bil <janbil9991@gmail.com>
2026-01-27 19:01:23 +0100

    feat: add shared header skeleton

---
+++ tramwaj_wodny/common.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_H
+#define COMMON_H	
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // COMMON_H
